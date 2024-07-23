0dda83f7c116c8cb643f7004846eedff2ab96828
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-07-23 16:05:29 +0200

    POWI-224.dev: indication that key setting fields are required.

--- views/admin_twig/de/module_options.php
+++ views/admin_twig/de/module_options.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 $aLang = [
     'charset' => 'UTF-8',
     'SHOP_MODULE_GROUP_power_captcha_general'   => 'Allgemeine Einstellungen',
-    'SHOP_MODULE_power_captcha_ApiKey'         => 'API-Key',
-    'HELP_SHOP_MODULE_power_captcha_ApiKey'    => 'Gib deinen POWER CAPTCHA API-Key ein. Den API-Key findest du im <a href="https://power-captcha.com/mein-konto/api-keys/" target="_blank">API-Key Management</a>.', // TODO maybe add a link?
-    'SHOP_MODULE_power_captcha_SecretKey'      => 'Secret-Key',
-    'HELP_SHOP_MODULE_power_captcha_SecretKey' => 'Gib deinen POWER CAPTCHA Secret-Key ein. Den Secret-Key findest du im <a href="https://power-captcha.com/mein-konto/api-keys/" target="_blank">API-Key Management</a>.', // TODO maybe add a link?
+    'SHOP_MODULE_power_captcha_ApiKey'         => 'API-Key (erforderlich)',
+    'HELP_SHOP_MODULE_power_captcha_ApiKey'    => 'Gib deinen POWER CAPTCHA API-Key ein. Den API-Key findest du im <a href="https://power-captcha.com/mein-konto/api-keys/" target="_blank">API-Key Management</a>.',
+    'SHOP_MODULE_power_captcha_SecretKey'      => 'Secret-Key (erforderlich)',
+    'HELP_SHOP_MODULE_power_captcha_SecretKey' => 'Gib deinen POWER CAPTCHA Secret-Key ein. Den Secret-Key findest du im <a href="https://power-captcha.com/mein-konto/api-keys/" target="_blank">API-Key Management</a>.',
     // 'SHOP_MODULE_oemoduletemplate_GreetingMode_personal' => 'personal',
     // 'SHOP_MODULE_oemoduletemplate_BrandName' => 'Brand name',
     // 'SHOP_MODULE_oemoduletemplate_LoggerEnabled' => 'Enable logger',
--- views/admin_twig/en/module_options.php
+++ views/admin_twig/en/module_options.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 $aLang = [
     'charset' => 'UTF-8',
     'SHOP_MODULE_GROUP_power_captcha_general'   => 'General settings',
-    'SHOP_MODULE_power_captcha_api_key'         => 'API Key',
-    'HELP_SHOP_MODULE_power_captcha_api_key'    => 'Enter your POWER CAPTCHA API Key. Find your API Key in the <a href="https://power-captcha.com/en/my-account/api-keys/" target="_blank">API Key Management</a>.', // TODO maybe add a link?
-    'SHOP_MODULE_power_captcha_secret_key'      => 'Secret Key',
-    'HELP_SHOP_MODULE_power_captcha_secret_key' => 'Enter your POWER CAPTCHA Secret Key. Find your Secret Key in the <a href="https://power-captcha.com/en/my-account/api-keys/" target="_blank">API Key Management</a>.', // TODO maybe add a link?
+    'SHOP_MODULE_power_captcha_api_key'         => 'API Key (required)',
+    'HELP_SHOP_MODULE_power_captcha_api_key'    => 'Enter your POWER CAPTCHA API Key. Find your API Key in the <a href="https://power-captcha.com/en/my-account/api-keys/" target="_blank">API Key Management</a>.',
+    'SHOP_MODULE_power_captcha_secret_key'      => 'Secret Key (required)',
+    'HELP_SHOP_MODULE_power_captcha_secret_key' => 'Enter your POWER CAPTCHA Secret Key. Find your Secret Key in the <a href="https://power-captcha.com/en/my-account/api-keys/" target="_blank">API Key Management</a>.',
     // 'SHOP_MODULE_oemoduletemplate_GreetingMode_personal' => 'personal',
     // 'SHOP_MODULE_oemoduletemplate_BrandName' => 'Brand name',
     // 'SHOP_MODULE_oemoduletemplate_LoggerEnabled' => 'Enable logger',
