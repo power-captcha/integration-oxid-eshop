80f84e9ea597dd10f8f1a68eb5590a41defde874
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-07-24 12:05:12 +0200

    POWI-224.dev: highlight required settings.

--- views/admin_twig/de/module_options.php
+++ views/admin_twig/de/module_options.php
@@ -7,9 +7,9 @@ $aLang = [
 
     // General settings
     'SHOP_MODULE_GROUP_power_captcha_general'  => 'Allgemeine Einstellungen',
-    'SHOP_MODULE_power_captcha_ApiKey'         => 'API-Key (erforderlich)',
+    'SHOP_MODULE_power_captcha_ApiKey'         => 'API-Key <strong>(erforderlich)</strong>',
     'HELP_SHOP_MODULE_power_captcha_ApiKey'    => 'Gib deinen POWER CAPTCHA API-Key ein. Den API-Key findest du im <a href="https://power-captcha.com/mein-konto/api-keys/" target="_blank">API-Key Management</a>.',
-    'SHOP_MODULE_power_captcha_SecretKey'      => 'Secret-Key (erforderlich)',
+    'SHOP_MODULE_power_captcha_SecretKey'      => 'Secret-Key <strong>(erforderlich)</strong>',
     'HELP_SHOP_MODULE_power_captcha_SecretKey' => 'Gib deinen POWER CAPTCHA Secret-Key ein. Den Secret-Key findest du im <a href="https://power-captcha.com/mein-konto/api-keys/" target="_blank">API-Key Management</a>.',
     'SHOP_MODULE_power_captcha_CheckMode'      => 'Prüfmodus (optional)',
     'HELP_SHOP_MODULE_power_captcha_CheckMode' =>
