c4afb132e04b6cd54bedd14b54b2ac69d4033817
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-09-10 16:37:41 +0200

    POWI-224.dev: added email adress to metadata.php and removed TOOD comments.

--- metadata.php
+++ metadata.php
@@ -19,7 +19,7 @@ $aModule = [
         'en' => 'POWER CAPTCHA Integration for OXID eShop',
         'de' => 'POWER CAPTCHA Integration für OXID eShop'
     ],
-    'description' =>  [ // TODO check description
+    'description' =>  [
         'en' => 'POWER CAPTCHA protects your OXID eShop against bots and unauthorized persons. GDPR compliant!',
         'de' => 'POWER CAPTCHA schützt deinen OXID eShop gegen Bots und unberechtigte Personen. DSGVO-konform!'
     ], 
@@ -27,7 +27,7 @@ $aModule = [
     'version'     => '0.0.1',
     'author'      => 'POWER CAPTCHA',
     'url'         => 'https://power-captcha.com/en',
-    // 'email'       => '', // TODO email
+    'email'       => 'dev-team@power-captcha.com',
     'extend'      => [
         OxidEsales\Eshop\Core\ViewConfig::class => PowerCaptcha\OxidEshop\Core\ViewConfig::class,
         OxidEsales\Eshop\Application\Component\UserComponent::class => PowerCaptcha\OxidEshop\Component\UserComponent::class,
@@ -136,53 +136,5 @@ $aModule = [
             'type'        => 'str', 
             'value'       => ''
         ],
-
-        //TODO: add help texts for settings to explain possibilities and point out which ones only serve as example
-        // /** Main */
-        // [
-        //     'group'       => 'oemoduletemplate_main',
-        //     'name'        => 'oemoduletemplate_GreetingMode',
-        //     'type'        => 'select',
-        //     'constraints' => 'generic|personal',
-        //     'value'       => 'generic'
-        // ],
-        // [
-        //     'group' => 'oemoduletemplate_main',
-        //     'name'  => 'oemoduletemplate_BrandName',
-        //     'type'  => 'str',
-        //     'value' => 'Testshop'
-        // ],
-        // [
-        //     'group' => 'oemoduletemplate_main',
-        //     'name'  => 'oemoduletemplate_LoggerEnabled',
-        //     'type'  => 'bool',
-        //     'value' => false
-        // ],
-        // [
-        //     'group' => 'oemoduletemplate_main',
-        //     'name'  => 'oemoduletemplate_Timeout',
-        //     'type'  => 'num',
-        //     'value' => 30
-        //     //'value' => 30.5
-        // ],
-        // [
-        //     'group' => 'oemoduletemplate_main',
-        //     'name'  => 'oemoduletemplate_Categories',
-        //     'type'  => 'arr',
-        //     'value' => ['Sales', 'Manufacturers']
-        // ],
-        // [
-        //     'group' => 'oemoduletemplate_main',
-        //     'name'  => 'oemoduletemplate_Channels',
-        //     'type'  => 'aarr',
-        //     'value' => ['1' => 'de', '2' => 'en']
-        // ],
-        // [
-        //     'group'    => 'oemoduletemplate_main',
-        //     'name'     => 'oemoduletemplate_Password',
-        //     'type'     => 'password',
-        //     'value'    => 'changeMe',
-        //     'position' => 3
-        // ]
     ],
 ];
