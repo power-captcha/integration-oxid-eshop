c0605bd5db9e1b7fc5ba7ac63f708c202c9e59b6
Tim.Wittler <tim.wittler@uniiqueag.com>
2025-03-24 18:07:57 +0100

    POWI-262.dev: Changed module version to 2.0.0 for Oxid 7.1

--- metadata.php
+++ metadata.php
@@ -26,7 +26,7 @@ $aModule = [
         'de' => 'POWER CAPTCHA schützt deinen OXID eShop gegen Bots und unberechtigte Personen. DSGVO-konform!'
     ],
     'thumbnail'   => 'pictures/logo.png',
-    'version'     => '0.1.0',
+    'version'     => '2.0.0',
     'author'      => 'POWER CAPTCHA',
     'url'         => 'https://power-captcha.com/en',
     'email'       => 'dev-team@power-captcha.com',
